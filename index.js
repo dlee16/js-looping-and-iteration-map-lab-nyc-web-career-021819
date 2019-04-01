7581fcccd1d418fd25a30f6816c55f798221ac04
dlee16 <diana.lee116@gmail.com>
2019-03-31 23:00:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  return drivers.map(function(name) {
+    return name.toLowerCase();
+  });
+}
+
+function nameToAttributes(drivers){
+  return drivers.map(function(name){
+    const fullN = name.split(' ')
+    const firstN= fullN[0];
+    const lastN= fullN[fullN.length -1];
+    return {firstName: firstN, lastName: lastN }
+  });
+}
+
+function attributesToPhrase(drivers){
+  return drivers.map(function(name){
+    return `${name.name} is from ${name.hometown}`;
+  });
+}
